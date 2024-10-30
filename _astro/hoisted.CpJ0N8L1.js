e0d790bac0296f6060fb64840d71947167ef68dc
myupopov <93000608+myupopov@users.noreply.github.com>
2024-10-30 16:46:28 +0300

    Add files via upload

---
+++ _astro/hoisted.CpJ0N8L1.js
@@ -0,0 +1,1 @@
+import"./hoisted.3vT7O11j.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.getElementById("empty"),n=document.documentElement.clientHeight,e=908;n>e&&(t.style.height=`${document.documentElement.clientHeight-e}px`)});
